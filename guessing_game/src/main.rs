8a4a71dbc008d163249e76a5aa1817de33dbe96d
Prasanth Karri <prasanth.25k@gmail.com>
2020-04-12 08:32:17 +0530

    guessing_game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Enter a number");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("guessed number is {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
+}
